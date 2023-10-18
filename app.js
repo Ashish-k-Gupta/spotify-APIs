b81d1f1778ff46e707f4c2e4ac1ce59099f3f684
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2023-10-18 22:15:50 +0530

    started this project where I have to build something using spotify APIs let see what I can make

---
+++ app.js
@@ -0,0 +1,26 @@
+//imports
+const express = require('express')
+const app = express();
+const port = 8080;
+
+app.use(express.static('public'))
+app.use('/CSS', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/images', express.static(__dirname + 'public/images'))
+
+//static files
+// app.set('views', './views');
+// app.set('view engine', 'html');
+
+//This display my html files
+app.get('',(req, res)=> {
+    res.sendFile(__dirname + "/view/web.html")
+})
+
+app.get('/logged', (req, res) => {
+    res.sendFile(__dirname + "/views/logged.html")
+})
+
+//listen on port 8080
+
+app.listen (port, () => console.info ('listening on port ${port}'));
